dddada1b672177833ecc2a278dbd40b85d0f3a1b
John <jtommyok8+github@gmail.com>
2020-05-01 05:22:28 -0400

    adds base Space component

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Space from './Space';
 
 export default function App({ initialData }) {
   const [count, setCount] = useState(0);
---
+++ src/components/Space.js
@@ -0,0 +1,8 @@
+import React, {useState} from React
+
+const Space = (props) => {
+
+    return <button className="space"></button>
+}
+
+export default Space
