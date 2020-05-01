67686473b3538bc45246ecd21c7db2e923c3c745
John <jtommyok8+github@gmail.com>
2020-05-01 05:55:32 -0400

    displays 16 spaces

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
 import Space from './Space';
+import { range } from '../utils';
 
 export default function App({ initialData }) {
   const [count, setCount] = useState(0);
   return (
     <div>
-      <Space />
+      {range(1, 16).map((number) => (
+        <Space key={number} />
+      ))}
     </div>
   );
 }
