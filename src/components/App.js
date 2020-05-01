b7efa564b7c0283c1c23ce58c96b63b9d9b605b2
John <jtommyok8+github@gmail.com>
2020-05-01 06:07:06 -0400

    adds color to play buttons

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,23 @@ import React, { useState } from 'react';
 import Space from './Space';
 import { range } from '../utils';
 
+const colors = {
+  unused: 'lightgray',
+  used: 'lightgreen',
+  wrong: 'lightcoral',
+  candidate: 'deepskyblue',
+};
+
+const testColors = range(1, 16).map((number) =>
+  number % 2 == 0 ? colors.used : colors.unused
+);
+
 export default function App({ initialData }) {
   const [count, setCount] = useState(0);
   return (
     <div className="playArea">
       {range(1, 16).map((number) => (
-        <Space key={number} />
+        <Space key={number} color={testColors[number - 1]} />
       ))}
     </div>
   );
--- src/components/Space.js
+++ src/components/Space.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 
 const Space = (props) => {
-  return <button className="space"></button>;
+  return (
+    <button className="space" style={{ backgroundColor: props.color }}></button>
+  );
 };
 
 export default Space;
